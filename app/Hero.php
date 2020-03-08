8dd2ce5ba07cc7d2ad2ddc5ecc60884b9a5751e0
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2020-03-08 14:08:36 -0700

    Added soft deletes to Hero entity

--- app/Hero.php
+++ app/Hero.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hero extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'firstName', 'lastName', 'level', 'str', 'dex', 'int', 'exp', 'active'
     ];
