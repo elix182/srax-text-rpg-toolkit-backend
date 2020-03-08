d85f2ccdd4afd4fa7720348096ad688278caa238
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2020-03-07 18:59:06 -0800

    Added service for listing all heroes

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Hero;
+
 class HeroController extends Controller
 {
-    //
+    public function list(){
+        $heroes = Hero::all();
+        if($heroes->isEmpty()){
+            return response()->json([ 'message' => "There aren't any Heroes registered"]);
+        }
+        return response()->json($heroes);
+    }
 }
