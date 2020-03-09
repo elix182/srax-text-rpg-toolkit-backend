ae6b4df7120ae033d74b3c2437b7863641ece48e
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2020-03-08 20:10:44 -0700

    Added API endpoint for creating monsters

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 use App\Monster;
 use App\MonsterRace;
 use App\MonsterAbility;
@@ -30,7 +32,66 @@ class MonsterController extends Controller
     }
 
     public function create(Request $request){
+        $validator = Validator::make($request->all(),
+        [
+            'name' => 'required|string',
+            'raceId' => 'required|integer',
+            'abilities' => 'present|array',
+            'str' => 'required|integer',
+            'dex' => 'required|integer',
+            'int' => 'required|integer',
+            'picture' => 'required|string',
+        ]);
+        if($validator->fails()){
+            return response()->json(["message" => "The monster request lacks data"], 400);
+        }
+        //Check if the selected data exists
+        $race = MonsterRace::find($request->raceId);
+        if($race == null){
+            return response()->json(["message" => "Monster Race not found"], 404);
+        }
+        // Check if te selected abilities are valid and fetch the abilities entities from the DB
+        $abilities = [];
+        foreach($request->abilities as $abilityId){
+            $ability = MonsterAbility::find($abilityId);
+            if($ability == null){
+                return response()->json(['message' => 'Monster ability not found'], 404);
+            }
+            if($race->availableAbilities()->find($abilityId) == null){
+                return response()->json(["message" => "A $race->name can't use $ability->name"], 400);
+            }
+            $abilities[] = $ability;
+        }
+        //Finally check if the data is valid
+        if($request->str <= 0 || $request->str > 100){
+            return response()->json(["message" => "STR cannot be lower or equal than 0 or greater than 100"], 400);
+        }
+        if($request->dex <= 0 || $request->dex > 100){
+            return response()->json(["message" => "DEX cannot be lower or equal than 0 or greater than 100"], 400);
+        }
+        if($request->int <= 0 || $request->int > 100){
+            return response()->json(["message" => "INT cannot be lower or equal than 0 or greater than 100"], 400);
+        }
+        if(count($abilities) == 0){
+            return response()->json(["message" => "Your monster needs at least 1 ability"], 400);
+        }
+        //Actual creation of the monster with initial state
+        $level = (count($abilities) * 2) - 1;
+        $monster = new Monster();
+        $monster->name = $request->name;
+        $monster->str = $request->str;
+        $monster->dex = $request->dex;
+        $monster->int = $request->int;
+        $monster->picture = $request->picture;
+        $monster->monster_race_id = $race->id;
+        $monster->level = $level;
+        $monster->save();
+        foreach($abilities as $ability){
+            $monster->abilities()->save($ability);
+        }
+        $monster->save();
 
+        return response()->json(["message" => "$race->name $monster->name level $level created successfully"]);
     }
 
     public function delete(Int $id){
