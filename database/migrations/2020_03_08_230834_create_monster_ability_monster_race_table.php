d4e87e3b54f9b03344d77ba3c1f88ed0c99c5a92
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2020-03-08 18:46:24 -0700

    Added fixtures for Monster Race & Monster Ability

--- database/migrations/2020_03_08_230834_create_monster_ability_monster_race_table.php
+++ database/migrations/2020_03_08_230834_create_monster_ability_monster_race_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMonsterRaceMonsterAbilityTable extends Migration
+class CreateMonsterAbilityMonsterRaceTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateMonsterRaceMonsterAbilityTable extends Migration
      */
     public function up()
     {
-        Schema::create('monster_race_monster_ability', function (Blueprint $table) {
-            $table->unsignedBigInteger('monster_race_id');
+        Schema::create('monster_ability_monster_race', function (Blueprint $table) {
             $table->unsignedBigInteger('monster_ability_id');
+            $table->unsignedBigInteger('monster_race_id');
             $table->foreign('monster_race_id')
               ->references('id')
               ->on('monster_races');
@@ -32,6 +32,6 @@ class CreateMonsterRaceMonsterAbilityTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('monster_race_monster_ability');
+        Schema::dropIfExists('monster_ability_monster_race');
     }
 }
