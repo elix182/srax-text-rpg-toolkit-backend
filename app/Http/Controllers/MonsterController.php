a46041323fa9e30aeba712d62b3baf433ff7e641
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2020-03-08 19:30:35 -0700

    Added API endpoint for listing all monsters

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Monster;
+use App\MonsterRace;
+use App\MonsterAbility;
 
 class MonsterController extends Controller
 {
     public function list(){
-
+        $monsters = Monster::all();
+        if($monsters->isEmpty()){
+            return response()->json(['message'=>"There aren't any Monsters registered"], 404);
+        }
+        return response()->json($monsters);
     }
 
     public function random(){
